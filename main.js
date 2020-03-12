3419a2b83f6e55c0092a50f6c9cf6d9280afff13
Matteo Bettiol <bettiol.m01@gmail.com>
2020-03-13 00:25:58 +0100

    ship drawn

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const SHIP_SIZE = 30; // height in pixels
 
 var ship = {
     x: canv.width / 2,
-    y: canv.heigth / 2,
+    y: canv.height / 2,
     r: SHIP_SIZE / 2, // radius
     a: 90 / 180 * Math.PI // direction of movement
 }
@@ -17,10 +17,25 @@ setInterval(update, 1000 / FPS);
 function update() {
 
     //draw space
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, canv.width, canv.height);
 
     // draw ship
     ctx.strokeStyle = "#FFFFFF";
     ctx.lineWidth = SHIP_SIZE / 20;
+    ctx.beginPath();
+    ctx.moveTo(
+        ship.x + ship.r * Math.cos(ship.a) * 1.5,
+        ship.y - ship.r * Math.sin(ship.a) * 1.5
+    );
+    ctx.lineTo(
+        ship.x - ship.r * (Math.cos(ship.a) + Math.sin(ship.a)),
+        ship.y + ship.r * (Math.sin(ship.a) - Math.cos(ship.a))
+    );
+    ctx.lineTo(
+        ship.x - ship.r * (Math.cos(ship.a) - Math.sin(ship.a)),
+        ship.y + ship.r * (Math.sin(ship.a) + Math.cos(ship.a))
+    );
+    ctx.closePath();
+    ctx.stroke();
 }
