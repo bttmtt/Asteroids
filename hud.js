e5f3ecb3a98b068ac9ddd248b0ebf53bf4099a5f
Matteo Bettiol <bettiol.m01@gmail.com>
2020-03-22 22:14:12 +0100

    best score message improved

--- hud.js
+++ hud.js
@@ -6,7 +6,7 @@ function drawHud() {
 
     // draw the score
     ctx.textAlign = "right";
-    ctx.fillStyle = score < bestScore ? "#ffffff" : "#ffff00";
+    ctx.fillStyle = score <= bestScore ? "#ffffff" : "#ffff00";
     ctx.font = TEXT_SIZE + "px 'Lato', sans-serif";
     ctx.fillText(score, canv.width - SHIP_SIZE / 2, SHIP_SIZE * 1.75);
 
@@ -14,9 +14,8 @@ function drawHud() {
     if (score > bestScore && textNewBestAlpha > 0) {
         ctx.textAlign = "center";
         ctx.fillStyle = "rgba(255, 255, 0, " + textNewBestAlpha + ")";
-        ctx.font = (TEXT_SIZE * 0.5) + "px 'Lato', sans-serif";
-        ctx.fillText("NEW BEST SCORE!", canv.width / 2, SHIP_SIZE * 1.75);
+        ctx.font = (TEXT_SIZE * 1.5) + "px 'Lato', sans-serif";
+        ctx.fillText("NEW BEST SCORE!", canv.width / 2, SHIP_SIZE * 5);
         textNewBestAlpha -= (1.0 / TEXT_FADE_TIME / FPS);
     }
-
 }
